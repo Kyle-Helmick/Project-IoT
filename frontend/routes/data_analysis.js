959dd9bf41eea5a543fe931f070abd3a969dea94
Kyle-Helmick <kyhe6996@colorado.edu>
2018-02-25 04:22:37 -0700

    Even newer things

--- frontend/routes/data_analysis.js
+++ frontend/routes/data_analysis.js
@@ -1,31 +1,39 @@
 var express = require('express');
 var router = express.Router();
-var MongoClient = require('mongodb').MongoClient;
+const MongoClient = require('mongodb').MongoClient;
+const asser = require('assert');
 
-var url = "mongodb://raspberry:HackCUIV@172.31.94.195:27017/Project-IoT"
+const url = "mongodb://raspberry:HackCUIV@172.31.94.195:27017/Project-IoT";
+const dbname = "Project-IoT";
 
-MongoClient.connect(url, function(err, client) {
-	assert.equal(null, err);
-	console.log("Connected securely to server");
+(async function() {
+	let client;
 
-	const db = client.db("Project-IoT");
+	try {
+		client = await MongoClient.connect(url);
+		console.log("Connected securely to server");
 
-	const temp = db.collection("Temperature");
-	const humid = db.collection("Humidity");
-	const light = db.collection("Light");
+		const db = client.db(dbname);
 
-	var tempArray = await temp.find({}).next(function(err, documents) {return documents});
-	console.log(tempArray)
-	var humidArray = await humid.find({}).next(function(err, documents) {return documents});
-	console.log(humidArray)
-	var lightArray = await light.find({}).next(function(err, documents) {return documents});
-	console.log(lightArray)
+		const temp = db.collection("Temperature");
+		const humid = db.collection("Humidity");
+		const light = db.collection("Light");
 
-	router.get('/', async (req, res, next) => {
-		tempArray = await collections[0].find().toArray( docs => {
-			res.render('data_analysis', {temp: docs})
+		var tempArray = await temp.find({}).next(function(err, documents) {return documents});
+		console.log(tempArray)
+		var humidArray = await humid.find({}).next(function(err, documents) {return documents});
+		console.log(humidArray)
+		var lightArray = await light.find({}).next(function(err, documents) {return documents});
+		console.log(lightArray)
+
+		router.get('/', async (req, res, next) => {
+			tempArray = await collections[0].find().toArray( docs => {
+				res.render('data_analysis', {temp: docs})
+			});
 		});
-	});
+	} catch (err) {
+		console.log(err.stack)
+	}
 });
 
 module.exports = router;
