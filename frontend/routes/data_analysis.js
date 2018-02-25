36d3f61a8da496dd38eb783033fe62d6459e5444
Kyle Helmick <Kyle.Helmick@colorado.edu>
2018-02-25 05:03:01 -0700

    Screw mongo 3.*

--- frontend/routes/data_analysis.js
+++ frontend/routes/data_analysis.js
@@ -1,40 +1,27 @@
 var express = require('express');
 var router = express.Router();
 const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
 
 const url = "mongodb://raspberry:HackCUIV@172.31.94.195:27017/Project-IoT";
 const dbname = "Project-IoT";
 
-(async function() {
-	let client;
-
-	try {
-		client = await MongoClient.connect(url);
-		console.log("Connected securely to server");
-
-		const db = client.db(dbname);
-
+MongoClient.connect(url)
+	.then( db => {
 		const temp = db.collection("Temperature");
 		const humid = db.collection("Humidity");
 		const light = db.collection("Light");
-
+		return [temp, humid, light]
+	})
+	.then( collection => {
 		router.get('/', async (req, res, next) => {
-
-			var tempArray = await temp.find({}).toArray();
-			console.log(tempArray)
-			var humidArray = await humid.find({}).toArray();
-			console.log(humidArray)
-			var lightArray = await light.find({}).toArray();
-			console.log(lightArray)
-
-			res.render('data_analysis', {temp: docs})
+			tempArray = await collection[0].find().toArray();
+			humidArray = await collection[1].find().toArray();
+			lightArray = await collection[2].find().toArray();
+			res.render('data_analysis', {temp: tempArray, humid: humidArray, light: lightArray});
 		});
-
-	} catch (err) {
-		console.log(err.stack)
-	}
-	client.close();
-});
+	})
+	.catch( err => {
+		console.log(err);
+	});
 
 module.exports = router;
