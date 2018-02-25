53c3b4692a0347baa51ce4a4bd8a3f8168f386d1
Kyle-Helmick <kyhe6996@colorado.edu>
2018-02-25 08:35:25 -0700

    update?

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,4 @@
-var express = require('express')();
+var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -12,6 +12,8 @@ var developers = require('./routes/developers')
 var data_analysis = require('./routes/data_analysis')
 
 var app = express();
+
+app.io = require( 'socket.io' )();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
