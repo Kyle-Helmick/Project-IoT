cacee87f02e23e8466737a26f08f31420440a312
ComputerBear <sean.harrison@colorado.edu>
2018-02-25 02:06:45 -0700

    actually accessing the right collections now

--- frontend/routes/data_analysis.js
+++ frontend/routes/data_analysis.js
@@ -6,17 +6,33 @@ var url = "mongodb://localhost:27017/Project-IoT";
 
 MongoClient.connect(url)
 	.then( db => {
-		return db.collection('Inverntory');
+		return db.collection('Temperature');
 	})
 	.then( collection => {
 		router.get('/', async (req, res, next) => {
 			tempArray = await collection.find().toArray();
-			humidArray = await collection.find().toArray();
-			lightArray = await collection.find().toArray();
 			res.render('index', {temp: tempArray, title: 'temps over time'});
+		});
+
+	.then( db => {
+		return db.collection('Humidity');
+	})
+	.then( collection => {
+		router.get('/', async (req, res, next) => {
+			humidArray = await collection.find().toArray();
 			res.render('index', {humid: humidArray, title: 'humidity over time'});
+		});
+
+	.then( db => {
+		return db.collection('Light');
+	})
+	.then( collection => {
+		router.get('/', async (req, res, next) => {
+			lightArray = await collection.find().toArray();
 			res.render('index', {light: lightArray, title: 'light over time'});
 		});
+
+
 
 		
 
