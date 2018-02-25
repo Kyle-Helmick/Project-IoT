23ac871b21f6f71c61f7c6345a72636a9ef58702
Kyle-Helmick <kyhe6996@colorado.edu>
2018-02-25 04:16:48 -0700

    trying new things

--- frontend/routes/data_analysis.js
+++ frontend/routes/data_analysis.js
@@ -4,27 +4,28 @@ var MongoClient = require('mongodb').MongoClient;
 
 var url = "mongodb://raspberry:HackCUIV@172.31.94.195:27017/Project-IoT"
 
-MongoClient.connect(url)
-	.then( client => {
-		var db = client.db("Project-IoT");
-		var temp = db.collection("Temperature");
-		var humid = db.collection("Humidity");
-		var light = db.collection("Light");
+MongoClient.connect(url, function(err, client) {
+	assert.equal(null, err);
+	console.log("Connected securely to server");
 
-		return [temp,humid,light];
-	})
-	.then( collections => {
-		router.get('/', async (req, res, next) => {
-			tempArray = await collections[0].find().toArray( docs => {
-				res.render('data_analysis', {temp: docs})
-			});
-			humidArray = await collections[1].find().toArray();
-			lightArray = await collections[2].find().toArray();
-			res.render('data_analysis', {temp: tempArray, humid: humidArray, light: lightArray});
+	const db = client.db("Project-IoT");
+
+	const temp = db.collection("Temperature");
+	const humid = db.collection("Humidity");
+	const light = db.collection("Light");
+
+	var tempArray = await temp.find({}).next(function(err, documents) {return documents});
+	console.log(tempArray)
+	var humidArray = await humid.find({}).next(function(err, documents) {return documents});
+	console.log(humidArray)
+	var lightArray = await light.find({}).next(function(err, documents) {return documents});
+	console.log(lightArray)
+
+	router.get('/', async (req, res, next) => {
+		tempArray = await collections[0].find().toArray( docs => {
+			res.render('data_analysis', {temp: docs})
 		});
-	})
-	.catch(err => {
-		console.log(err);
 	});
+});
 
 module.exports = router;
